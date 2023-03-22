d941895e086936bc0781a5d6acc5cbf5cb5775f5
Opointr <debd92@gmail.com>
2023-03-22 22:09:29 +0530

    fix nvm path

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -xe
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
-\. /home/ec2-user/.nvm/.nvm.sh
+\. /home/ec2-user/.nvm/nvm.sh
 \. /home/ec2-user/.bashrc
 
 nvm alias default v12.7.0
