c2c59af8eaf3dd15ef634ad1600b0002e8366bb6
Opointr <debd92@gmail.com>
2023-03-22 20:43:16 +0530

    appspec runs as ec-user, use sudo to create logs folder

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,6 +3,7 @@
 echo "start app"
 source /home/ec2-user/.bash_profile 
 cd /home/ec2-user/app
-mkdir -p logs
+sudo mkdir -p logs
+sudo chown ec2-user:ec2-user logs
 npm run start 
 echo "start app complete"
